421de094114e8cfb998b25d407804f48fda3d3cf
Laurel Butler <laurelbutler@Laurels-MBP.attlocal.net>
2019-02-26 15:35:34 -0500

    updated shopping list

--- index.js
+++
@@ -1,9 +0,0 @@
-function catCarousel() {
-    $('.thumbnail').on('click', function(event) {
-        const targetCat = $(this).find('img').attr('src');
-        const otherCat = $(this).find('img').attr('alt');
-        $('.hero img').attr('src', targetCat).attr('alt', otherCat);
-    });
-}
-
-$(catCarousel);
